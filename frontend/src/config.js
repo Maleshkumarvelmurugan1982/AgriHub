12a9cb783d7bd6de56cf1d1c23abf2aae16679ec
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2025-08-31 00:32:03 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-const API_URL = "https://agrihub-1.onrender.com"; // your Render backend URL
+const API_URL = "https://agrihub-1.onrender.com"; // Render backend
 export default API_URL;
