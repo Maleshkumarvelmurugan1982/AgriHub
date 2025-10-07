7e537f46b9b8266def63f9140623bc439f4bca0e
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2025-10-07 16:42:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://agri-hub-lful.vercel.app"
+      "https://agri-hub-lful.vercel.app",
       "https://agri-hub-th8q.vercel.app"
     ],
     credentials: true,
