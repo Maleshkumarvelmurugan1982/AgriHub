84a08c865b20bcdfd1b4ca30e722784b4ec0106c
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2025-11-05 18:46:55 +0530

    Update Navbar.js

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        {/* Logo */}
+        <Link className="navbar-brand" to="/" replace>
           <img
             src={process.env.PUBLIC_URL + "/Navbar/icon.png"}
             alt="Logo"
             className="navbar-icon"
           />
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -23,68 +25,30 @@ function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item home">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-            </li>
-
-            <li className="nav-item menu dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="/"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+          {/* Government Badge + Login/Register */}
+          <ul className="navbar-nav align-items-center">
+            {/* Government Badge */}
+            <li className="nav-item me-3">
+              <Link 
+                to="/GovernmentPage" 
+                replace 
+                className="badge bg-success text-white p-2" 
+                style={{ fontSize: "0.9rem", cursor: "pointer", textDecoration: "none" }}
               >
-                Menu
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a className="dropdown-item" href="/farmer">
-                    Farmer
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/seller">
-                    Seller
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/deliveryman">
-                    Deliveryman
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/GovernmentPage">
-                    Government
-                  </a>
-                </li>
-              </ul>
+                Government
+              </Link>
             </li>
-
-            <li className="nav-item about">
-              <a className="nav-link" href="#about-section">
-                About
-              </a>
-            </li>
-          </ul>
-
-          <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="login" href="/login">
+              <Link className="login" to="/login" replace>
                 Login
-              </a>
+              </Link>
             </li>
-
             <li className="nav-item">
-              <a className="register" href="/register">
+              <Link className="register" to="/register" replace>
                 Register
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
