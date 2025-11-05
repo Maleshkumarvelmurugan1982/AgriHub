24f1287e0171b5280abe02df081791bfd2dee3de
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2025-11-05 19:15:09 +0530

    Implement conditional rendering based on login state
    Added login state management to Navbar component.

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
+  // Check login based on localStorage flag
+  const isGovLoggedIn = localStorage.getItem("govLoggedIn") === "true";
+  
+  // to detect current page route
+  const location = useLocation();
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top">
       <div className="container-fluid">
+
         {/* Logo */}
         <Link className="navbar-brand" to="/" replace>
           <img
@@ -14,6 +21,7 @@ function Navbar() {
             className="navbar-icon"
           />
         </Link>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -25,31 +33,62 @@ function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-          {/* Government Badge + Login/Register */}
+
           <ul className="navbar-nav align-items-center">
-            {/* Government Badge */}
-            <li className="nav-item me-3">
-              <Link 
-                to="/GovernmentPage" 
-                replace 
-                className="badge bg-success text-white p-2" 
-                style={{ fontSize: "0.9rem", cursor: "pointer", textDecoration: "none" }}
-              >
-                Government
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="login" to="/login" replace>
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="register" to="/register" replace>
-                Register
-              </Link>
-            </li>
+            
+            {/* Show Government Badge only if NOT logged in */}
+            {!isGovLoggedIn && (
+              <li className="nav-item me-3">
+                <Link
+                  to="/GovernmentPage"
+                  replace
+                  className="badge bg-success text-white p-2"
+                  style={{
+                    fontSize: "0.9rem",
+                    cursor: "pointer",
+                    textDecoration: "none",
+                  }}
+                >
+                  Government
+                </Link>
+              </li>
+            )}
+
+            {/* Show Login/Register only if NOT logged in */}
+            {!isGovLoggedIn && (
+              <>
+                <li className="nav-item">
+                  <Link className="login" to="/login" replace>
+                    Login
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link className="register" to="/register" replace>
+                    Register
+                  </Link>
+                </li>
+              </>
+            )}
+
+            {/* Show Logout only when logged in */}
+            {isGovLoggedIn && (
+              <li className="nav-item">
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => {
+                    localStorage.removeItem("govLoggedIn");
+                    window.location.href = "/";
+                  }}
+                >
+                  Logout
+                </button>
+              </li>
+            )}
+
           </ul>
         </div>
       </div>
