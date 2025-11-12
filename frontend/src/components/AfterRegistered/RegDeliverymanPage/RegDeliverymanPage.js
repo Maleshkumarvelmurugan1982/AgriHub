392825d231f92b7f83452460a2c3561eb4e59558
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2025-11-12 15:06:53 +0530

    Update RegDeliverymanPage.js

--- frontend/src/components/AfterRegistered/RegDeliverymanPage/RegDeliverymanPage.js
+++ frontend/src/components/AfterRegistered/RegDeliverymanPage/RegDeliverymanPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import NavbarRegistered from "../../NavbarRegistered/NavbarRegistered";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronRight,
